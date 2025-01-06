d42212ab2277a453f402c9a7abf92e65ced2cf17
shougal <xdq9qa@virginia.edu>
2025-01-06 11:03:25 -0500

    added code to AuthContext.js to manage authentication state globally

---
+++ src/context/AuthContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+// check https://youtu.be/WpIDez53SK4?si=2_0j4gdnGlkGlHFu if not working
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
