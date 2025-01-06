2b363c30d70045650b9ca26a221fb8f0b69ba066
shougal <xdq9qa@virginia.edu>
2025-01-06 11:05:09 -0500

    Wrapping the App with the Context and Browser Router for index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // Optional, for global styles
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter } from "react-router-dom";
+
 
 
 
 // Render the App component into the root div in index.html
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Wrapping the App with the Context and Browser Router
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
