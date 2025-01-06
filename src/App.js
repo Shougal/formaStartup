48eba9e2d8c978da12c719af7de4e557af2ce5bc
shougal <xdq9qa@virginia.edu>
2025-01-06 11:14:56 -0500

    removed router from app.js, compile issue fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -14,23 +14,21 @@ import {faUser} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/photographer" element={<Photographer />} />
-            <Route path="/barber" element={<Barber />} />
-            <Route path="/nail-stylist" element={<NailStylist />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/join-us" element={<JoinUs />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/photographer" element={<Photographer />} />
+        <Route path="/barber" element={<Barber />} />
+        <Route path="/nail-stylist" element={<NailStylist />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/join-us" element={<JoinUs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </main>
+    <Footer />
+  </div>
   );
 }
 
