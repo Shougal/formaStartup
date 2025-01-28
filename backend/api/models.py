b7be81bc3218913eb70b85c0ad7ad71d6d8437d5
shougal <xdq9qa@virginia.edu>
2025-01-28 17:09:30 -0500

    Groups and permission to solve reverse accessor issue. Enforced unique email as username_field

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,3 +10,46 @@ class Provider(AbstractUser):
     prices = models.JSONField()
     location = models.CharField(max_length=255)
     is_approved = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, blank=False)  # Enforce email uniqueness
+    # class Meta:
+    #     db_table = "provider"
+    #     app_label = "api"
+    #     permissions = ("")
+    #TODO: Add user and group persmissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='providers',
+        verbose_name= "Provider Group"
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='providers_permissions',
+        verbose_name= "Provider Permissions",
+    )
+    # USERNAME_FIELD tells django that the unique identifier should be the email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'specialty', 'location', 'availability', 'prices']
+
+
+
+
+class Customer(AbstractUser):
+    location = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, blank=False)  # Enforce email uniqueness
+    # TODO: Add user and group persmissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customers',
+        verbose_name= "Customer Group"
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customer_permissions',
+        verbose_name= "Customer Permissions",
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'location']
+
+"""
+CREATE A META CLASS FOR PERMISSION AND GROUPS"""
+
