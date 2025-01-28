371d69672e54fecd93d31390df7030e699e63d7b
shougal <xdq9qa@virginia.edu>
2025-01-28 17:32:07 -0500

    added urls tp registration pages

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from api.views import CustomerRegistration, ProviderRegistration
+from api.views import register_provider, register_customer
 
 # THESE VIEWS ARE PREBUILT VIEWS THAT ALLOWS TO ACCESS OUR ACCESS AND REFRESH TOKENS
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -24,6 +25,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/provider/', register_provider, name='register_provider'),
+    path('register/customer/', register_customer, name='register_customer'),
     # path('register/customer/', CustomerRegistration.as_view(), name='customer-registration'),
     # path('register/provider/', ProviderRegistration.as_view(), name='provider-registration'),
     path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
