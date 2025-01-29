e13bbe6b6dd57971551e2bc704921c071728c007
shougal <xdq9qa@virginia.edu>
2025-01-28 21:31:10 -0500

    Provided fields that can be seen and edited in admin Panel. Specified permission - only super Users

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,30 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 from .models import Provider, Customer
 
 class ProviderAdmin(BaseUserAdmin):
     model = Provider
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('specialty', 'availability', 'prices', 'location', 'is_approved')}),
+        ('Provider Info', {'fields': ('specialty', 'availability', 'prices', 'is_approved')}),
     )
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Additional Info', {
+        ('Provider Info', {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'specialty', 'location', 'availability', 'prices', 'is_approved'),
+            'fields': ('username', 'email', 'password1', 'password2', 'specialty', 'availability', 'prices', 'location',
+                       'is_approved'),
         }),
     )
+
+    def has_module_permission(self, request):
+        return request.user.is_superuser  # Only allow superusers
 
 class CustomerAdmin(BaseUserAdmin):
     model = Customer
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('location',)}),
+        ('Customer Info', {'fields': ('location',)}),
     )
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Additional Info', {
+        ('Provider Info', {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'location'),
+            'fields': ('username', 'email', 'password1', 'password2', 'location'),
         }),
     )
 
+    def has_module_permission(self, request):
+        return request.user.is_superuser  # Only allow superusers
+
 admin.site.register(Provider, ProviderAdmin)
 admin.site.register(Customer, CustomerAdmin)
+
+# Hide Groups from the admin panel
+admin.site.unregister(Group)
