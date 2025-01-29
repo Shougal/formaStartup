83bfac02b558b110f442829585e1e985f4ae58a6
shougal <xdq9qa@virginia.edu>
2025-01-28 21:29:20 -0500

    Post methods and form saving is added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .forms import ProviderSignUpForm, CustomerSignUpForm
 
 def register_provider(request):
     if request.method == 'POST':
         form = ProviderSignUpForm(request.POST)
         if form.is_valid():
-            provider = form.save()
-            #TODO:Handle post-save actions here, like logging user in or redirecting
+            form.save()
+            # TODO:Handle post-save actions here, like logging user in or redirecting
+            # TODO: Change redirect
             return redirect('index')
     else:
         form = ProviderSignUpForm()
@@ -16,67 +18,29 @@ def register_customer(request):
     if request.method == 'POST':
         form = CustomerSignUpForm(request.POST)
         if form.is_valid():
-            customer = form.save()
-            # TODO:Handle post-save actions here
+            form.save()
+            # TODO:Handle post-save actions here, like logging user in or redirecting
+            # TODO: Change redirect
             return redirect('index')
     else:
         form = CustomerSignUpForm()
     return render(request, 'registration/register_customer.html', {'form': form})
-
-
+#TODO: Remove index - and ad Home page instead
 def index(request):
     return render(request, 'index.html')
 
-
-
-# from rest_framework import status
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from .serializers import UserSerializer, ProviderSerializer
-# from rest_framework.permissions import AllowAny
+"""TODO: Reimplement this by adding correct redirect"""
+# @login_required
+# @user_passes_test(lambda user: user.is_superuser)
+# def admin_dashboard(request):
+#     return render(request, 'admin_dashboard.html')
 #
-# class CustomerRegistration(APIView):
-#     permission_classes = [AllowAny]
+# @login_required
+# @user_passes_test(lambda user: user.is_provider)
+# def provider_dashboard(request):
+#     return render(request, 'provider_dashboard.html')
 #
-#     def get(self, request, *args, **kwargs):
-#         serializer = UserSerializer()  # No data is passed, so an empty form is initialized
-#         return Response(serializer.data)  # Returns a Response with serializer data, which DRF uses to render the form
-#
-#     def post(self, request):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-# class ProviderRegistration(APIView):
-#     def post(self, request):
-#         serializer = ProviderSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-#
-#
-# # from django.shortcuts import render
-# # from django.contrib.auth.models import User
-# # from rest_framework import generics
-# # from .serializers import UserSerializer
-# # from rest_framework.permissions import IsAuthenticated, AllowAny
-# # # Creating a simple view that allows us to create a user
-# # # Create your views here.
-# #
-# # # Creating a registration form:
-# # #generics.CreateAPIView is a generic view that is built into django
-# # # that will automatically handle creating a new user or a new object
-# # class CreateUserView(generics.CreateAPIView):
-# #     queryset = User.objects.all() # List of all diff object that will be looked at
-# #     # when creating a new one - to make sure we don't create a user that already exists
-# #     serializer_class = UserSerializer #Serializer class that tells this view what kind of data to accept to
-# #     # make a new user
-# #
-# #     # Allowing anyone to use this view to create a new user
-# #     # even if they are not authenticated
-# #     permission_classes = (AllowAny,)
+# @login_required
+# @user_passes_test(lambda user: user.is_customer)
+# def customer_dashboard(request):
+#     return render(request, 'customer_dashboard.html')
