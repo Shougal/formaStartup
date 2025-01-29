dda3ee1cf58d9ea575618bf65277e2818f1895b5
shougal <xdq9qa@virginia.edu>
2025-01-28 21:29:58 -0500

    Specified is user is provider or customer in forms.py

--- backend/api/forms.py
+++ backend/api/forms.py
@@ -3,11 +3,27 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Provider, Customer
 
 class ProviderSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = Provider
-        fields = ['email', 'username', 'specialty', 'location', 'availability', 'prices']
+        fields = ['email', 'username', 'password1', 'password2', 'specialty', 'availability', 'prices']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_provider = True
+        user.is_customer = False
+        if commit:
+            user.save()
+        return user
 
 class CustomerSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = Customer
-        fields = ['email', 'username', 'location']
+        fields = ['email', 'username', 'password1', 'password2', 'location']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.is_provider = False
+        if commit:
+            user.save()
+        return user
