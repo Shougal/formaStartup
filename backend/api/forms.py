fcd209a6042501a50f3d3236a0923629c1d23f46
shougal <xdq9qa@virginia.edu>
2025-01-30 13:45:39 -0500

    Commented out forms.py since I will be using serializers with react instead

--- backend/api/forms.py
+++ backend/api/forms.py
@@ -1,29 +1,29 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import Provider, Customer
-
-class ProviderSignUpForm(UserCreationForm):
-    class Meta:
-        model = Provider
-        fields = ['email', 'username', 'password1', 'password2', 'specialty', 'availability', 'prices']
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_provider = True
-        user.is_customer = False
-        if commit:
-            user.save()
-        return user
-
-class CustomerSignUpForm(UserCreationForm):
-    class Meta:
-        model = Customer
-        fields = ['email', 'username', 'password1', 'password2', 'location']
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_customer = True
-        user.is_provider = False
-        if commit:
-            user.save()
-        return user
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from .models import Provider, Customer
+#
+# class ProviderSignUpForm(UserCreationForm):
+#     class Meta:
+#         model = Provider
+#         fields = ['email', 'username', 'password1', 'password2', 'specialty', 'availability', 'prices']
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.is_provider = True
+#         user.is_customer = False
+#         if commit:
+#             user.save()
+#         return user
+#
+# class CustomerSignUpForm(UserCreationForm):
+#     class Meta:
+#         model = Customer
+#         fields = ['email', 'username', 'password1', 'password2', 'location']
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.is_customer = True
+#         user.is_provider = False
+#         if commit:
+#             user.save()
+#         return user
