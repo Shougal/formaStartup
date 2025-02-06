6f4c4ff64ce4432f90c406e061ae9765e961f625
shougal <xdq9qa@virginia.edu>
2025-02-05 20:53:48 -0500

    Updated comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Optional, for global styles
+import "./index.css"; //  for global styles
 
 
 
