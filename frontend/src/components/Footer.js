87adf46b9845016658be9cdd4eef1ce6423c8dcd
shougal <xdq9qa@virginia.edu>
2025-02-06 10:12:35 -0500

    Sent Contact us form in the footer to Forma email upon submit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,23 @@ import React from "react";
 import "./Footer.css";
 import logo from "../assets/FormaLogo.jpeg"; 
 import { Link } from "react-router-dom";
+import emailjs from 'emailjs-com';
+
 
 function Footer() {
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs.sendForm('service_ojccfvo', 'template_pdcx4h8', event.target, 'avXKEkyUfZ7-Ga4N_')
+      .then((result) => {
+          console.log('Email successfully sent!', result.text);
+          alert("Message sent successfully!");
+      }, (error) => {
+          console.log('Failed to send email.', error.text);
+          alert("Failed to send message!");
+      });
+  };
   return (
     <footer className="bg-white py-3 text-black">
       <div className="container">
@@ -22,13 +37,13 @@ function Footer() {
             <Link to="/about" className="footer-button5 btn btn-light rounded-pill btn-sm me-3 bg-transparent border-0">About</Link>
           </div>
           <div className="col-md-4">
-            <form className="footer-form">
+            <form className="footer-form" onSubmit={sendEmail}>
               {/*TODO: send to forma email */}
               <h1>Contact Us</h1>
               <div className="row">
                 <div className="col-md-6 mb-3">
                   <label htmlFor="firstName" className="form-label">First name *</label>
-                  <input type="text" className="form-control" id="firstName" placeholder="Enter your first name" required />
+                  <input type="text" className="form-control" id="firstName" placeholder="Enter your first name" name="name" required />
                 </div>
                 <div className="col-md-6 mb-3">
                   <label htmlFor="lastName" className="form-label">Last name *</label>
@@ -38,16 +53,16 @@ function Footer() {
               <div className="row">
                 <div className="col-md-6 mb-3">
                   <label htmlFor="email" className="form-label">Email *</label>
-                  <input type="email" className="form-control" id="email" placeholder="Enter your email" required />
+                  <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email" required />
                 </div>
                 <div className="col-md-6 mb-3">
                   <label htmlFor="subject" className="form-label">Subject</label>
-                  <input type="text" className="form-control" id="subject" placeholder="Enter subject" />
+                  <input type="text" className="form-control" id="subject" name="subject" placeholder="Enter subject" />
                 </div>
               </div>
               <div className="mb-3">
                 <label htmlFor="message" className="form-label">Message *</label>
-                <textarea className="form-control" id="message" rows="3" placeholder="Your message" required></textarea>
+                <textarea className="form-control" id="message" name="message" rows="3" placeholder="Your message" required></textarea>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
