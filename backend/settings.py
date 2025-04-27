2cb55b695ded66c075485b41d5fda2a834cf0760
shougal <xdq9qa@virginia.edu>
2025-04-26 21:36:35 -0400

    Fix: Correct app paths to backend.api

--- backend/settings.py
+++ backend/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'backend.api',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders'
