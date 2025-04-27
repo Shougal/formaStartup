635ac86ddcabb3b3a63e653f25ee8dab5b8205c8
shougal <xdq9qa@virginia.edu>
2025-04-26 22:22:06 -0400

    Fixing home page url

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import HttpResponse
 from django.views.generic import TemplateView
@@ -39,5 +39,6 @@ urlpatterns = [
     #TODO: api-auth?
     path('api/auth/', include('rest_framework.urls')),
     path('api/', include('backend.api.urls')),
-    path('', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name="index.html")),
 ]
