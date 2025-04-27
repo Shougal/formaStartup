6ab89bc76371c975234bc8d3996470968b4fa28e
shougal <xdq9qa@virginia.edu>
2025-04-26 23:45:56 -0400

    Fix: final backend urls setup for React frontend

--- backend/settings.py
+++ backend/settings.py
@@ -151,7 +151,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend' / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -222,12 +222,15 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATIC_URL = 'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # BASE_DIR refers to outer backend/
+STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'build' / 'static']
+
+# STATICFILES_DIRS = [BASE_DIR / 'static']  # BASE_DIR refers to outer backend/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
