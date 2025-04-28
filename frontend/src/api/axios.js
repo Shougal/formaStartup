65c5cd29540a813a6e8d6ba546da946a502a44a8
shougal <xdq9qa@virginia.edu>
2025-04-28 14:18:38 -0400

    Fix CSRF token attachment in axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -20,9 +20,26 @@ const axiosInstance = axios.create({
   },
 });
 
+// // Adding a request interceptor
+// axiosInstance.interceptors.request.use(config => {
+//
+//   const storedData = localStorage.getItem('user');
+//   if (storedData) {
+//     const { accessToken } = JSON.parse(storedData);
+//     if (accessToken) {
+//       config.headers['Authorization'] = `Bearer ${accessToken}`; // Append token to headers
+//     }
+//   }
+//   return config;
+// }, error => {
+//   // Do something with request error
+//   return Promise.reject(error);
+// });
+//
+// export default axiosInstance;
 // Adding a request interceptor
 axiosInstance.interceptors.request.use(config => {
-
+  // 1. Attach Authorization Header
   const storedData = localStorage.getItem('user');
   if (storedData) {
     const { accessToken } = JSON.parse(storedData);
@@ -30,10 +47,32 @@ axiosInstance.interceptors.request.use(config => {
       config.headers['Authorization'] = `Bearer ${accessToken}`; // Append token to headers
     }
   }
+
+  // 2. Attach CSRF Token Header (for unsafe methods)
+  const csrfToken = getCookie('csrftoken');
+  if (csrfToken && ['post', 'put', 'patch', 'delete'].includes(config.method)) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+
   return config;
 }, error => {
-  // Do something with request error
   return Promise.reject(error);
 });
+
+// Helper function to get CSRF cookie
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
 export default axiosInstance;
